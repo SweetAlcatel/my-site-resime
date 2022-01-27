f920550c150da789ca7bbf24be18a242d23e417e
sweetalcatel <mrneey@gmail.com>
2022-01-27 11:31:15 +0300

    sime fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/', router);
 
 app.use(express.static(path.join(__dirname, 'Front/dist')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Front/dist', 'index.html'));
 });
 
