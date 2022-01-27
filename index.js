6d6338d15995dfe286d5b258136b6ccf9447f0f2
sweetalcatel <mrneey@gmail.com>
2022-01-27 14:08:38 +0300

    fix of CORS

--- index.js
+++ index.js
@@ -17,20 +17,7 @@ const app = express();
 
 app.use(express.json());
 
-const whitelist = ["http://localhost:3000/getData"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-
-app.use(cors(corsOptions))
+app.use(cors());
 
 app.use('/', router);
 
